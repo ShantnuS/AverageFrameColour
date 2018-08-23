3dd24f19bd568de1004fffa1a61690757ce5ffa6
ShantnuS <shantnu@outlook.com>
2018-08-23 17:20:20 +0100

    main method

--- average-colours.py
+++ average-colours.py
@@ -9,6 +9,12 @@ import os
 import shutil
 from PIL import Image, ImageStat
 
+download_folder = "cache"
+download_name = "test"
+frame_folder = os.path.join(download_folder,"frames")
+
+video_url = "https://www.youtube.com/watch?v=hMILDJ_l5ik"
+
 #Get average colour of frames
 def get_average_colour(image_name, colour_list):
   image = Image.open(frame_folder+"/"+image_name)
@@ -16,36 +22,33 @@ def get_average_colour(image_name, colour_list):
   colour_list.append(average)
   print(image_name + ": " + str(average))
   
-video_url = "https://www.youtube.com/watch?v=hMILDJ_l5ik"
+def main():
+  try:
+    shutil.rmtree(download_folder)
+  except FileNotFoundError:
+    print(download_folder + " folder does not exist. Creating one!")
 
-download_folder = "cache"
-download_name = "test"
-frame_folder = os.path.join(download_folder,"frames")
+  #Make sure folders exist
+  os.makedirs(download_folder, exist_ok=True)
+  os.makedirs(frame_folder, exist_ok=True)
 
-try:
-  shutil.rmtree(download_folder)
-except FileNotFoundError:
-  print(download_folder + " folder does not exist. Creating one!")
+  #Download video
+  video = YouTube(video_url)
+  stream = video.streams.filter(mime_type="video/mp4",res="360p").all()[0]
+  stream.download(download_folder, download_name)
 
-#Make sure folders exist
-os.makedirs(download_folder, exist_ok=True)
-os.makedirs(frame_folder, exist_ok=True)
-
-#Download video
-video = YouTube(video_url)
-stream = video.streams.filter(mime_type="video/mp4",res="360p").all()[0]
-stream.download(download_folder, download_name)
-
-#Get frames from video (once every second)
-vidcap = cv2.VideoCapture(download_folder+"/"+download_name+".mp4")
-fps = vidcap.get(cv2.CAP_PROP_FPS)
-success,image = vidcap.read()
-count = 0
-colour_list = []
-while success:
-  if count % round(fps) == 0:
-    frame_name = "frame%d.jpg" % count
-    cv2.imwrite(frame_folder+"/"+frame_name, image)     # save frame as JPEG file
-    get_average_colour(frame_name, colour_list)
+  #Get frames from video (once every second)
+  vidcap = cv2.VideoCapture(download_folder+"/"+download_name+".mp4")
+  fps = vidcap.get(cv2.CAP_PROP_FPS)
   success,image = vidcap.read()
-  count += 1
+  count = 0
+  colour_list = []
+  while success:
+    if count % round(fps) == 0:
+      frame_name = "frame%d.jpg" % count
+      cv2.imwrite(frame_folder+"/"+frame_name, image)     # save frame as JPEG file
+      get_average_colour(frame_name, colour_list)
+    success,image = vidcap.read()
+    count += 1
+
+if __name__ == "__main__": main()
